01073064df739a8da42c242aeb29f190b072c97d
3XiangyiKong3 <xkong@ucsd.edu>
2023-09-28 14:48:49 -0700

    open graph meta tag added

--- src/lib/seo.ts
+++ src/lib/seo.ts
@@ -1,40 +1,129 @@
-import { NextSeo } from 'next-seo';
+// import { NextSeo } from 'next-seo';
+// import { useRouter } from 'next/router';
+
+// import type { ComponentProps } from 'react';
+
+// export function useSeoProps(
+// 	props: Partial<ComponentProps<typeof NextSeo>> = {},
+// ): Partial<ComponentProps<typeof NextSeo>> {
+// 	const router = useRouter();
+
+// 	const title = 'Xiangyi ─ developer';
+// 	const description = "Hey 👋 I'm Xiangyi, a developer";
+
+// 	return {
+// 		title,
+// 		description,
+// 		canonical: `https://xiangyikong.com/${router.asPath}`,
+// 		openGraph: {
+// 			title,
+// 			description,
+// 			site_name: 'Xiangyi Kong',
+// 			url: `https://xiangyikong.com/${router.asPath}`,
+// 			type: 'website',
+// 			images: [
+// 				{
+// 					url: 'https://xiangyikong.com/banner.png',
+// 					alt: description,
+// 					width: 1280,
+// 					height: 720,
+// 				},
+// 			],
+// 		},
+// 		// twitter: {
+// 		// 	cardType: 'summary_large_image',
+// 		// 	handle: '@nurodev',
+// 		// 	site: '@nurodev',
+// 		// },
+// 		...props,
+// 	};
+// }
+
+// import { DeepPartial } from 'utility-types';  // Or define DeepPartial yourself if you prefer
 import { useRouter } from 'next/router';
+import { ComponentProps } from 'react';
+import { NextSeo } from 'next-seo';
+import { DEFAULT_SEO } from './seoConfig'; // Importing from the configuration file
 
-import type { ComponentProps } from 'react';
 
-export function useSeoProps(
+type DeepPartial<T> = {
+	[P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P];
+  };
+  
+
+  export function useSeoProps(
 	props: Partial<ComponentProps<typeof NextSeo>> = {},
-): Partial<ComponentProps<typeof NextSeo>> {
+	dynamicTitle?: string,
+	dynamicDescription?: string,
+  ): Partial<ComponentProps<typeof NextSeo>> {
 	const router = useRouter();
-
-	const title = 'Xiangyi ─ developer';
-	const description = "Hey 👋 I'm Xiangyi, a developer";
-
+  
+	const title = dynamicTitle || DEFAULT_SEO.title;
+	const description = dynamicDescription || DEFAULT_SEO.description;
+  
+	const canonicalUrl = `https://xiangyikong.com${router.asPath}`;
+  
 	return {
+	  title,
+	  description,
+	  canonical: canonicalUrl,
+	  openGraph: {
 		title,
 		description,
-		canonical: `https://xiangyikong.com/${router.asPath}`,
-		openGraph: {
-			title,
-			description,
-			site_name: 'Xiangyi Kong',
-			url: `https://xiangyikong.com/${router.asPath}`,
-			type: 'website',
-			images: [
-				{
-					url: 'https://xiangyikong.com/banner.png',
-					alt: description,
-					width: 1280,
-					height: 720,
-				},
-			],
+		site_name: DEFAULT_SEO.site_name,
+		url: canonicalUrl,
+		type: 'website',
+		locale: DEFAULT_SEO.locale,
+		images: [
+		  {
+			url: `${canonicalUrl}/banner.png`,
+			alt: description,
+			width: 1280,
+			height: 720,
+		  },
+		],
+	  },
+	  additionalMetaTags: [
+		{
+		  name: 'keywords',
+		  content: DEFAULT_SEO.keywords,
 		},
-		// twitter: {
-		// 	cardType: 'summary_large_image',
-		// 	handle: '@nurodev',
-		// 	site: '@nurodev',
-		// },
-		...props,
+		{
+		  name: 'author',
+		  content: DEFAULT_SEO.author,
+		},
+	  ],
+	//   metaTags: { // Not native to next-seo; may need custom handling
+	// 	keywords: DEFAULT_SEO.keywords,
+	// 	author: DEFAULT_SEO.author,
+	//   },
+	  ...props,
 	};
-}
+  }
+
+// export function useSeoProps(
+//   props: DeepPartial<ComponentProps<typeof NextSeo>> = {}
+// ): DeepPartial<ComponentProps<typeof NextSeo>> {
+//   const router = useRouter();
+
+//   return {
+//     title: DEFAULT_SEO.title,
+//     description: DEFAULT_SEO.description,
+//     canonical: `https://xiangyikong.com${router.asPath}`,
+//     openGraph: {
+//       title: DEFAULT_SEO.title,
+//       description: DEFAULT_SEO.description,
+//       site_name: DEFAULT_SEO.site_name,
+//       url: `https://xiangyikong.com${router.asPath}`,
+//       type: 'website',
+//       images: [DEFAULT_SEO.banner],
+//     },
+//     // twitter: {
+//     //  cardType: 'summary_large_image',
+//     //  handle: '@handle',
+//     //  site: '@site',
+//     // },
+//     ...props,
+//   };
+// }
+
---
+++ src/lib/seoConfig.ts
@@ -0,0 +1,24 @@
+// seoConfig.ts
+export const DEFAULT_SEO = {
+    title: 'Xiangyi (Tony) Kong',
+    description: "Hey 👋 I'm Xiangyi, a developer",
+    site_name: 'Xiangyi Kong',
+    banner: {
+      url: 'https://xiangyikong.com/banner.png',
+      alt: "Hey 👋 I'm Xiangyi, a developer",
+      width: 1280,
+      height: 720,
+    },
+    locale: 'en_US',
+    keywords: 'developer, xiangyi, portfolio, xiangyi kong, xiangyi(tony) kong, tony kong, xiangyi kong portfolio, \
+    xiangyi kong developer, xiangyi kong software engineer, xiangyi kong software developer, xiangyi kong software engineer portfolio, \
+    xiangyi kong software developer portfolio, xiangyi kong developer portfolio, xiangyi kong software engineer website, \
+    xiangyi kong software developer website, xiangyi kong software engineer portfolio website, \
+    xiangyi kong software developer portfolio website, xiangyi kong developer portfolio website, \
+    xiangyi kong software engineer personal website, xiangyi kong software developer personal website, \
+    xiangyi kong software engineer portfolio personal website, xiangyi kong software developer portfolio personal website, \
+    xiangyi kong developer portfolio personal website, xiangyi kong software engineer personal website, \
+    xiangyi kong software developer personal website, xiangyi kong software engineer portfolio personal website, \
+    xiangyi kong software developer portfolio personal website, xiangyi kong developer portfolio personal website',
+    author: 'Xiangyi Kong',
+  };
